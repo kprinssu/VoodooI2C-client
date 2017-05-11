1ef038f1e90e9ec93db3f3e414b13fe31f923ae5
Kishor Prins <me@68x.ca>
2017-05-11 11:47:39 -0400

    Remove warnings

--- updd.c
+++ updd.c
@@ -1,6 +1,9 @@
 #include "upddapi.h"
 #include "updd.h"
+#include "CGEventAdditions.h"
 
+#include <string.h>
+#include <stdio.h>
 #include <unistd.h>
 
 bool keep_running = true;
@@ -28,9 +31,17 @@ void inject_touch(struct csgesture_softc* sc) {
 
 	// get the first avaliable multi touch device
 	HTBDEVICE device = TBApiGetRelativeDevice(0);
-	printf("Injecting for %d %d\n", sc->x[0], sc->y[0]);
+	printf("Injecting for %d %d\n", sc->x[1], sc->y[1]);
 	if(sc->x[0] != -1 && sc->y[0] != -1) {
 		TBApiInjectTouch(device, sc->x[0], sc->y[0], 0, true);
+		//TBApiInjectTouch(device, sc->x[1], sc->y[1], 1, true);
+		//TBApiInjectTouch(device, sc->x[2], sc->y[2], 2, true);
+		//TBApiInjectTouch(device, sc->x[3], sc->y[3], 3, true);
+	} else {
+		TBApiInjectTouch(device, sc_old.x[0], sc_old.y[0], 0, false);
+		//TBApiInjectTouch(device, sc_old.x[1], sc_old.y[1], 1, false);
+		//TBApiInjectTouch(device, sc_old.x[2], sc_old.y[2], 2, false);
+		//TBApiInjectTouch(device, sc_old.x[3], sc_old.y[3], 3, false);
 	}
 	
 	memcpy(&sc_old, sc, sizeof(struct csgesture_softc));
